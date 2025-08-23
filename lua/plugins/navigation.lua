dbb832051dd8faf973e388c5a315586fca98589a
Logan Lessen <loganlessen@gmail.com>
2025-08-22 20:25:23 -0600

    Added leap.nvim

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -61,5 +61,20 @@ return {
                 preview = true,
             }
         }
+    },
+    {
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").set_default_mappings()
+            require("leap").opts.preview_filter =
+                function(ch0, ch1, ch2)
+                    return not (
+                        ch1:match('%s') or
+                        ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
+                    )
+                end
+            require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+            require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+        end
     }
 }
