545e2989b60016182b75440d9ca5828b89e21a40
Logan <loganlessen@gmail.com>
2025-09-02 13:44:38 -0600

    Added nvim-surround

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -13,7 +13,7 @@ return {
             view_options = {
                 show_hidden = true,
             },
-            skip_confirm_for_simple_edits = false,
+            skip_confirm_for_simple_edits = true,
         }
     },
 }
