bb8cff1e7129d3f4eeeb68c4d5d64ae2420e5ece
Logan Lessen <loganlessen@gmail.com>
2025-11-09 20:34:55 -0700

    feat(nvim): add subway surfers

---
+++ nvim/dot-config/nvim/lua/plugins/brainrot.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "Facel3ss1/subway-surfers.nvim",
+        cmd = { "SubwaySurfers" },
+        config = true,
+        -- Download gameplay video after install/update
+        build = ":SubwaySurfers download",
+    }
+}
