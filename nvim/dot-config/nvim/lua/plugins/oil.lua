86dce113678b63ce4f338b6c0df0b1a7b8ab13bf
Logan Lessen <loganlessen@gmail.com>
2026-01-18 13:20:09 -0700

    feat(nvim): QoL oil changes and add detail view

--- nvim/dot-config/nvim/lua/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,4 @@
+local detail = false
 return {
     {
         "stevearc/oil.nvim",
@@ -9,15 +10,32 @@ return {
             },
             keymaps = {
                 ["q"] = { "actions.close", mode = "n" },
+                ["gd"] = {
+                    desc = "Toggle file detail view",
+                    callback = function()
+                        detail = not detail
+                        if detail then
+                            require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+                        else
+                            require("oil").set_columns({ "icon" })
+                        end
+                    end
+                }
             },
             view_options = {
                 show_hidden = true,
                 is_always_hidden = function(name)
-                    return vim.endswith(name, ".uid")
+                    return name == ".." or name == ".git"
                 end
+            },
+            lsp_file_methods = {
+                autosave_changes = "unmodified"
             },
             skip_confirm_for_simple_edits = true,
             confirmation = {
+                border = "rounded"
+            },
+            keymaps_help = {
                 border = "rounded"
             }
         }
