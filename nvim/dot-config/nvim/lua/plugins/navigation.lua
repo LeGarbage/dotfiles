8f3c50c406c5f268f67b2d65dadecf657384aa1d
Logan Lessen <loganlessen@gmail.com>
2026-04-03 14:25:27 -0600

    feat(nvim): remove bento

--- nvim/dot-config/nvim/lua/plugins/navigation.lua
+++ nvim/dot-config/nvim/lua/plugins/navigation.lua
@@ -42,30 +42,13 @@ return {
         config = function()
             vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
             vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
-            require("leap").opts.preview_filter =
-                function(ch0, ch1, ch2)
-                    return not (
-                        ch1:match('%s') or
-                        ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
-                    )
-                end
+            require('leap').opts.preview = function(ch0, ch1, ch2)
+                return not (
+                    ch1:match('%s')
+                    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+                )
+            end
             require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
         end
-    },
-    {
-        "serhez/bento.nvim",
-        opts = {
-            main_keymap = "\\",
-            lock_char = "󰌾",
-            map_last_accessed = true,
-            ui = {
-                floating = {
-                    max_rendered_buffers = 8,
-                }
-            },
-            highlights = {
-                previous = "ErrorMsg"
-            }
-        }
     }
 }
