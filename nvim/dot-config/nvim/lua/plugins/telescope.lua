881c3c985a64551c7b13eee1548e86d9e9e7373d
Logan Lessen <loganlessen@gmail.com>
2026-04-03 21:17:25 -0600

    feat(nvim): improve telescope

--- nvim/dot-config/nvim/lua/plugins/telescope.lua
+++ nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,51 +1,15 @@
 return {
     'nvim-telescope/telescope.nvim',
+    version = "*",
     dependencies = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         'nvim-telescope/telescope-ui-select.nvim',
     },
     config = function()
-        local actions = require("telescope.actions")
-        local make_entry = require("telescope.make_entry")
         local telescope = require("telescope")
+        local actions = require("telescope.actions")
 
-        -- Preserve original generator
-        -- Returns a function that formats each entry
-        local original_gen = make_entry.gen_from_buffer
-
-        -- Custom entry generator
-        local function buffer_entry(opts)
-            -- Use the default generator as a baseline
-            local gen = original_gen(opts)
-
-            -- Entry generator
-            return function(entry)
-                local original = gen(entry)
-
-                -- Extract the flags (active, hidden, alt, etc.)
-                local flags = original.indicator
-
-                -- Fix telescope's interpretation of hidden and active buffers
-                if flags:find("a") and #vim.fn.win_findbuf(original.bufnr) == 0 then
-                    flags = flags:gsub("a", " ")
-                end
-
-                -- Sub the symbols
-                flags = flags:gsub("%%", "θ")
-                flags = flags:gsub("h", "β")
-                flags = flags:gsub("a", "α")
-                flags = flags:gsub("#", "ψ")
-                flags = flags:gsub("+", "δ")
-                flags = flags:gsub("=", "ϱ")
-
-                -- Inject the new flags
-                original.indicator = flags
-                return original
-            end
-        end
-        -- Overwrite the old telescope buffer list generator with custom
-        make_entry.gen_from_buffer = buffer_entry
         telescope.setup({
             defaults = {
                 mappings = {
@@ -53,6 +17,16 @@ return {
                         ["<esc>"] = actions.close
                     },
                 },
+            },
+
+            pickers = {
+                buffers = {
+                    sort_lastused = true,
+                    sort_mru = true
+                },
+                find_files = {
+                    hidden = true
+                }
             },
 
             extensions = {
