bda61adf8bf61e42a00de87e87db41ddb8132ee0
Logan Lessen <loganlessen@gmail.com>
2026-04-05 13:43:56 -0600

    feat(nvim): remove brainrot and orgmode

--- nvim/dot-config/nvim/lua/plugins/brainrot.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "petermused/subway-surfers.nvim",
-        cmd = { "SubwaySurfers" },
-        config = true,
-        -- Download gameplay video after install/update
-        build = ":SubwaySurfers download",
-    }
-}
