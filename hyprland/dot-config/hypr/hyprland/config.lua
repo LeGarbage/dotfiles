3bab77e8384fec10ddd9cde528d6bb9063858949
Logan Lessen <loganlessen@gmail.com>
2026-05-26 22:30:01 -0600

    feat(hypridle): use new dispatch syntax

--- hyprland/dot-config/hypr/hyprland/config.lua
+++ hyprland/dot-config/hypr/hyprland/config.lua
@@ -9,8 +9,6 @@ hl.config({
             inactive_border = "rgb(5c6370)"
         },
 
-        resize_on_border = true,
-
         -- Please see https://wiki.hypr.land/Configuring/Tearing/ before you turn this on
         allow_tearing = false,
 
@@ -34,7 +32,7 @@ hl.config({
         }
     },
     misc = {
-        force_default_wallpaper = 0, -- Set to 0 or 1 to disable the anime mascot wallpapers
+        force_default_wallpaper = 0,  -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
         focus_on_activate = true,
         disable_splash_rendering = true
