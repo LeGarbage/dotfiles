6b054d36a29f7b7f9d7548a7339165552478264d
Logan Lessen <loganlessen@gmail.com>
2026-08-11 20:48:36 -0600

    feat(hyprland): vrr

--- hyprland/dot-config/hypr/hyprland/config.lua
+++ hyprland/dot-config/hypr/hyprland/config.lua
@@ -35,7 +35,8 @@ hl.config({
         force_default_wallpaper = 0,  -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
         focus_on_activate = true,
-        disable_splash_rendering = true
+        disable_splash_rendering = true,
+        vrr = 2
     },
     xwayland = {
         force_zero_scaling = true
